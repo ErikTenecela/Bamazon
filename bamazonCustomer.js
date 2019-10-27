d4b51947f48a2842a549e6358f722eaf801159d6
Erik Tenecela <eriktenecela13@gmail.com>
2019-10-27 00:22:28 -0400

    progress with purchase/stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,12 +2,16 @@ const mysql = require('promise-mysql');
 
 const config = require('./config')
 
+const inquirer = require('inquirer')
 
 const Table = require('cli-table2');
 
+
+
 async function createConnection() {
 
-    const connect = await mysql.createConnection(config);
+    let connect = await mysql.createConnection(config);
+    query(connect);
     connect.query(`SELECT * FROM products`, (err, res) => {
         if (err) throw err;
 
@@ -32,4 +36,44 @@ async function createConnection() {
 
 
 }
+
+async function query(connect) {
+    const ask = await inquirer
+        .prompt({
+            name: "Product-to-buy",
+            type: "input",
+            message: "Please enter the Product id you wish to purchase "
+        })
+    const answer = ask["Product-to-buy"]
+    connect.query("SELECT * FROM products WHERE item_id=?", answer, (err, res) => {
+        if (err) throw err;
+        if (res.length === 0) {
+            console.log("The ID you chose doesn't exists please type a valid ID.")
+        } else {
+            console.log(`Thank you for your purchase.`)
+            const units = inquirer
+                .prompt({
+                    name: "Units",
+                    type: "input",
+                    message: "Please type the amount of units you want to purchase this item."
+                })
+
+            const unitsAnswer = units["Units"]
+            connect.query("UPDATE products SET stock_quantity = ? ", unitsAnswer, (err, res) => {
+                if (err) throw err;
+                if (res.length === 0) {
+                    console.log(`Please type a valid stock number`)
+                } else {
+                    console.log(`its working`)
+                }
+            })
+        }
+    })
+
+
+
+}
+
+
+
 createConnection()
